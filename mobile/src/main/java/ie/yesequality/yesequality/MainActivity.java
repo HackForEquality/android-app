128b03cda8acf4d8edc82b234712d1eba798d826
Dean Harrison <DeanH544@gmail.com>
2015-03-28 18:57:31 +0000

    WORK now...

--- mobile/src/main/java/ie/yesequality/yesequality/MainActivity.java
+++ mobile/src/main/java/ie/yesequality/yesequality/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-
 public class MainActivity extends FragmentActivity {
 
     private ListView mDrawerList;
