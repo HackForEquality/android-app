329c2a64a187df0f136c0620e387669c1d7078a7
Dean Harrison <DeanH544@gmail.com>
2015-03-28 17:35:10 +0000

    blarg1 the beginning

---
+++ mobile/src/main/java/InformationFragment.java
@@ -0,0 +1,15 @@
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InformationFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_information, container, false);
+        return rootView;
+    }
+}
