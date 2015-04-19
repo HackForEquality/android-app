3b8d8d4e7d78409530c1dcaf4b1b159f4ddfef05
josmas <josmasflores@gmail.com>
2015-04-19 18:32:35 +0100

    persist selections for reminders

--- mobile/src/main/java/ie/yesequality/yesequality/NotificationActivity.java
+++ mobile/src/main/java/ie/yesequality/yesequality/NotificationActivity.java
@@ -1,6 +1,8 @@
 package ie.yesequality.yesequality;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -16,6 +18,9 @@ import butterknife.InjectView;
 
 public class NotificationActivity extends Activity {
 
+    private static final String PREFS = "REMINDERS";
+    public static final String ON_DAY = "on_day";
+    public static final String ON_DAY_BEFORE = "on_day_before";
     @InjectView(R.id.switchDayBefore) Switch dayBefore;
     @InjectView(R.id.switchOnDay) Switch onDay;
     @InjectView(R.id.okButton) Button okButton;
@@ -25,6 +30,11 @@ public class NotificationActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
         ButterKnife.inject(this);
+
+        final SharedPreferences notificationPrefs = getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = notificationPrefs.edit();
+        onDay.setChecked(notificationPrefs.getBoolean(ON_DAY, true));
+        dayBefore.setChecked(notificationPrefs.getBoolean(ON_DAY_BEFORE, true));
 
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,12 +46,12 @@ public class NotificationActivity extends Activity {
         onDay.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                editor.putBoolean(ON_DAY, isChecked);
+                editor.commit();
                 if (isChecked){
-                    //Register Alarm
                     registerAlarm(true);
                 }
                 else {
-                    //Cancel Alarm
                     cancelAlarm(true);
                 }
             }
@@ -50,12 +60,12 @@ public class NotificationActivity extends Activity {
         dayBefore.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                editor.putBoolean(ON_DAY_BEFORE, isChecked);
+                editor.commit();
                 if (isChecked){
-                    //Register Alarm
                     registerAlarm(false);
                 }
                 else {
-                    //Cancel Alarm
                     cancelAlarm(false);
                 }
             }
