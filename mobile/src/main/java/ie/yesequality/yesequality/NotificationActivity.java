11e59efa6f9af9b185f78d0e7001d01df5b0ca60
josmas <josmasflores@gmail.com>
2015-04-20 00:06:39 +0100

    adds alarms for the day before and day of voting

--- mobile/src/main/java/ie/yesequality/yesequality/NotificationActivity.java
+++ mobile/src/main/java/ie/yesequality/yesequality/NotificationActivity.java
@@ -1,18 +1,22 @@
 package ie.yesequality.yesequality;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import java.util.Calendar;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import ie.yesequality.yesequality.broadcastreceivers.NotificationReceiver;
 
 
 public class NotificationActivity extends Activity {
@@ -20,6 +24,13 @@ public class NotificationActivity extends Activity {
     private static final String PREFS = "REMINDERS";
     public static final String ON_DAY = "on_day";
     public static final String ON_DAY_BEFORE = "on_day_before";
+
+    private AlarmManager alarmManagerOnDay;
+    private AlarmManager alarmManagerOnDayBefore;
+    private PendingIntent alarmIntentOnDay;
+    private PendingIntent alarmIntentOnDayBefore;
+    private int intentIdOnDay;
+    private int intentIdOnDayBefore;
     @InjectView(R.id.switchDayBefore) Switch dayBefore;
     @InjectView(R.id.switchOnDay) Switch onDay;
 
@@ -69,24 +80,45 @@ public class NotificationActivity extends Activity {
     }
 
     private void registerAlarm(boolean onDay) {
+
+        alarmManagerOnDay = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        alarmManagerOnDayBefore = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, NotificationReceiver.class);
+        intent.putExtra(ON_DAY, onDay);
+
+        Calendar dayBeforeVotingDay = Calendar.getInstance();
+        dayBeforeVotingDay.set(Calendar.YEAR, 2015);
+        dayBeforeVotingDay.set(Calendar.MONTH, 4);
+        dayBeforeVotingDay.set(Calendar.DAY_OF_MONTH, 21);
+        dayBeforeVotingDay.set(Calendar.HOUR_OF_DAY, 10);
+        dayBeforeVotingDay.set(Calendar.MINUTE, 30);
+        dayBeforeVotingDay.set(Calendar.SECOND, 0);
+
         if (onDay){
-            // Register alarm for voting day
-            Log.d("NotificationActivity", "Registering Alarm for Voting day");
+            // Register alarm for voting day (before day + 1)
+            dayBeforeVotingDay.add(Calendar.DATE, 1);
+            intentIdOnDay = (int) System.currentTimeMillis();
+            alarmIntentOnDay = PendingIntent.getBroadcast(this, intentIdOnDay, intent, 0);
+            alarmManagerOnDay.set(AlarmManager.RTC_WAKEUP, dayBeforeVotingDay.getTimeInMillis(), alarmIntentOnDay);
         }
         else {
             // Register alarm for day before voting day
-            Log.d("NotificationActivity", "Registering Alarm for day before Voting day");
+            intentIdOnDayBefore = (int) System.currentTimeMillis();
+            alarmIntentOnDayBefore = PendingIntent.getBroadcast(this, intentIdOnDayBefore, intent, 0);
+            alarmManagerOnDayBefore.set(AlarmManager.RTC_WAKEUP, dayBeforeVotingDay.getTimeInMillis(), alarmIntentOnDayBefore);
         }
     }
 
     private void cancelAlarm(boolean onDay) {
         if (onDay){
-            // Canceling alarm for voting day
-            Log.d("NotificationActivity", "Canceling Alarm for Voting day");
+            if (alarmManagerOnDay != null) {
+                alarmManagerOnDay.cancel(alarmIntentOnDay);
+            }
         }
         else {
-            // Canceling alarm for day before voting day
-            Log.d("NotificationActivity", "Canceling Alarm for day before Voting day");
+            if (alarmManagerOnDayBefore != null) {
+                alarmManagerOnDayBefore.cancel(alarmIntentOnDayBefore);
+            }
         }
     }
 
