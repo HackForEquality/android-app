9786ebf8683c3cce5ac4dad66137e27b8c1c8c0d
josmas <josmasflores@gmail.com>
2015-04-20 22:27:31 +0100

    adds alarms on Boot intent

--- mobile/src/main/java/ie/yesequality/yesequality/NotificationActivity.java
+++ mobile/src/main/java/ie/yesequality/yesequality/NotificationActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CompoundButton;
@@ -24,13 +25,13 @@ public class NotificationActivity extends Activity {
     private static final String PREFS = "REMINDERS";
     public static final String ON_DAY = "on_day";
     public static final String ON_DAY_BEFORE = "on_day_before";
+    public static final int ALARM_ID_ON_DAY = 220315;
+    public static final int ALARM_ID_ON_DAY_BEFORE = 210315;
 
     private AlarmManager alarmManagerOnDay;
     private AlarmManager alarmManagerOnDayBefore;
     private PendingIntent alarmIntentOnDay;
     private PendingIntent alarmIntentOnDayBefore;
-    private int intentIdOnDay;
-    private int intentIdOnDayBefore;
     @InjectView(R.id.switchDayBefore) Switch dayBefore;
     @InjectView(R.id.switchOnDay) Switch onDay;
 
@@ -77,12 +78,13 @@ public class NotificationActivity extends Activity {
                 }
             }
         });
+
+        alarmManagerOnDay = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        alarmManagerOnDayBefore = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
     }
 
     private void registerAlarm(boolean onDay) {
 
-        alarmManagerOnDay = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
-        alarmManagerOnDayBefore = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(this, NotificationReceiver.class);
         intent.putExtra(ON_DAY, onDay);
 
@@ -97,27 +99,39 @@ public class NotificationActivity extends Activity {
         if (onDay){
             // Register alarm for voting day (before day + 1)
             dayBeforeVotingDay.add(Calendar.DATE, 1);
-            intentIdOnDay = (int) System.currentTimeMillis();
-            alarmIntentOnDay = PendingIntent.getBroadcast(this, intentIdOnDay, intent, 0);
+            alarmIntentOnDay = PendingIntent.getBroadcast(this, ALARM_ID_ON_DAY, intent, 0);
             alarmManagerOnDay.set(AlarmManager.RTC_WAKEUP, dayBeforeVotingDay.getTimeInMillis(), alarmIntentOnDay);
+            Log.i("NOTIFICATIONACTIVITY", "Setting up Alarm for voting day on: " + dayBeforeVotingDay.getTime());
         }
         else {
             // Register alarm for day before voting day
-            intentIdOnDayBefore = (int) System.currentTimeMillis();
-            alarmIntentOnDayBefore = PendingIntent.getBroadcast(this, intentIdOnDayBefore, intent, 0);
+            alarmIntentOnDayBefore = PendingIntent.getBroadcast(this, ALARM_ID_ON_DAY_BEFORE, intent, 0);
             alarmManagerOnDayBefore.set(AlarmManager.RTC_WAKEUP, dayBeforeVotingDay.getTimeInMillis(), alarmIntentOnDayBefore);
+            Log.i("NOTIFICATIONACTIVITY", "Setting up Alarm for day before voting day on: " + dayBeforeVotingDay.getTime());
         }
     }
 
     private void cancelAlarm(boolean onDay) {
+
+        Intent intent = new Intent(this, NotificationReceiver.class);
+        intent.putExtra(ON_DAY, onDay);
+
         if (onDay){
             if (alarmManagerOnDay != null) {
+                if (alarmIntentOnDay == null){
+                    alarmIntentOnDay = PendingIntent.getBroadcast(this, ALARM_ID_ON_DAY, intent, 0);
+                }
                 alarmManagerOnDay.cancel(alarmIntentOnDay);
+                Log.i("NOTIFICATIONACTIVITY", "Canceled Alarm for voting day");
             }
         }
         else {
             if (alarmManagerOnDayBefore != null) {
+                if (alarmIntentOnDayBefore == null){
+                    alarmIntentOnDayBefore = PendingIntent.getBroadcast(this, ALARM_ID_ON_DAY_BEFORE, intent, 0);
+                }
                 alarmManagerOnDayBefore.cancel(alarmIntentOnDayBefore);
+                Log.i("NOTIFICATIONACTIVITY", "Canceled Alarm for day before voting day");
             }
         }
     }
