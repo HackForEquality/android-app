cd90ecc0e30d7bc4ac01f84307109b7785739b8c
Jessy Conroy <jessyconroy@tapadoo.com>
2015-04-28 21:22:19 +0100

    added view pager indicator to MainActivity. Removed redundant code

--- mobile/src/main/java/ie/yesequality/yesequality/MainActivity.java
+++ mobile/src/main/java/ie/yesequality/yesequality/MainActivity.java
@@ -3,93 +3,136 @@ package ie.yesequality.yesequality;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.view.Menu;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
+
+import com.viewpagerindicator.CirclePageIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class MainActivity extends FragmentActivity {
 
-    private ListView mDrawerList;
-    private DrawerLayout mDrawerLayout;
-    private String[] mListItems;
-    private Button closeInfoScreen;
+
+    @InjectView(R.id.indicator)
+    protected CirclePageIndicator indicator;
+    @InjectView(R.id.pager)
+    protected ViewPager pager;
+
+    private PageAdapter mPageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
 
-        mListItems = getResources().getStringArray(R.array.listItems);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        closeInfoScreen = (Button) findViewById(R.id.closeInfoButton);
+        List<Fragment> fragments = getFragmentList();
 
-        // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_item_list, mListItems));
-        // Set the list's click listener
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-        closeInfoScreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
+        mPageAdapter = new PageAdapter(getSupportFragmentManager(), fragments);
+
+
+        pager.setAdapter(mPageAdapter);
+        pager.setPageTransformer(true, new DepthPageTransformer());
+
+        indicator.setViewPager(pager);
+
+
+
+    }
+
+    @OnClick(R.id.closeInfoButton)
+    protected void onCloseInfoClicked() {
+        finish();
+    }
+
+
+    private List<Fragment> getFragmentList() {
+        List<Fragment> fragments = new ArrayList<Fragment>();
+        fragments.add(InformationPagesFragment.newInstance(R.drawable.infoicon1,
+                R.string.information_page_string_two, R.string.information_page_string_three,
+                R.color.lilac, "", R.color.white));
+
+        fragments.add(InformationPagesFragment.newInstance(R.drawable.infoicon2,
+                R.string.information_page_string_two, R.string.information_page_string_four, R.color.dark_cyan, "",
+                R.color.white));
+
+        fragments.add(InformationPagesFragment.newInstance(R.drawable.infoicon3,
+                R.string.information_page_string_two, R.string.information_page_string_five,
+                R.color.navy, "", R.color.white));
+
+        fragments.add(InformationPagesFragment.newInstance(R.drawable.infoicon4,
+                R.string.information_page_string_two, R.string.information_page_string_six,
+                R.color.dark_lilac, "", R.color.white));
+
+        fragments.add(InformationPagesFragment.newInstance(R.drawable.infoicon5,
+                R.string.information_page_string_seven, R.string.information_page_string_eight,
+                R.color.dark_navy, "http://www.checktheregister.ie", R.color.white));
+
+        fragments.add(InformationPagesFragment.newInstance(R.drawable.infoicon6,
+                R.string.information_page_string_nine, R.string.information_page_string_ten,
+                R.color.dark_green, "https://www.yesequality.ie/?attachment_id=1283", R.color.white));
+
+        fragments.add(InformationPagesFragment.newInstance(R.drawable.infoicon7,
+                R.string.information_page_string_nine, R.string.information_page_string_eleven,
+                R.color.dark_magenta, "", R.color.white));
+
+        fragments.add(InformationPagesFragment.newInstance(R.drawable.infoicon8,
+                R.string.information_page_string_nine, R.string.information_page_string_twelve,
+                R.color.dark_red, "", R.color.white));
+
+        fragments.add(InformationPagesFragment.newInstance(R.drawable.infoicon9,
+                R.string.information_page_string_thirteen, R.string.information_page_string_fourteen,
+                R.color.green, "", R.color.white));
+
+        fragments.add(InformationPagesFragment.newInstance(R.drawable.ic_yes_color,
+                R.string.information_page_string_seventeen, R.string.information_page_string_eighteen,
+                R.color.white, "http://www.yesequality.ie", R.color.black));
+
+        return fragments;
+    }
+
+    public class DepthPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.75f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+
+            } else if (position <= 0) { // [-1,0]
+                // Use the default slide transition when moving to the left page
+                view.setAlpha(1);
+                view.setTranslationX(0);
+                view.setScaleX(1);
+                view.setScaleY(1);
+
+            } else if (position <= 1) { // (0,1]
+                // Fade the page out.
+                view.setAlpha(1 - position);
+
+                // Counteract the default slide transition
+                view.setTranslationX(pageWidth * -position);
+
+                // Scale the page down (between MIN_SCALE and 1)
+                float scaleFactor = MIN_SCALE
+                        + (1 - MIN_SCALE) * (1 - Math.abs(position));
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0);
             }
-        });
-
-        if (savedInstanceState == null) {
-            selectItem(0);
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    private void selectItem(int position) {
-        // Create a new fragment and specify the planet to show based on position
-
-        Fragment fragment = null;
-
-        switch (position) {
-            case 0:
-                fragment = new InformationFragment();
-                break;
-            case 1:
-                fragment = new MythsFragment();
-                break;
-            case 2:
-                fragment = new LocationsFragment();
-                break;
-            case 3:
-                fragment = new VotingGuideFragment();
-                break;
-        }
-
-        if (fragment != null) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
-        }
-
-        // Highlight the selected item, update the title, and close the drawer
-        mDrawerList.setItemChecked(position, true);
-        setTitle(mListItems[position]);
-        mDrawerLayout.closeDrawer(mDrawerList);
-    }
-
-    private class DrawerItemClickListener extends FragmentActivity implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
-            selectItem(position);
-        }
-    }
 
 }
